4977a7a4f7553000e0a7275d80df3204699b7b1d
Karan Mehta <kmehta883@gmail.com>
2024-10-16 19:23:00 +0100

    First and Final Commit

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+
+setup(name="Demo_Project",
+      version="0.0.1",
+      packages=find_packages())
---
+++ src/math_operation.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+#Write the below code to code to trigger workflow in github
+# write a yml file that would trigger workflow in github actions, tell me what things you would need?
